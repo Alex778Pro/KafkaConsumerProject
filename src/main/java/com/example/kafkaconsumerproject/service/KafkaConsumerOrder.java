dde1c498535dfeaa86eca884c9272422de0ec2ae
Alex778Pro <aleksander.terehin2014@yandex.ru>
2025-08-10 20:08:15 +0300

    Новые правки

--- src/main/java/com/example/kafkaconsumerproject/service/KafkaConsumerOrder.java
+++ src/main/java/com/example/kafkaconsumerproject/service/KafkaConsumerOrder.java
@@ -1,15 +1,18 @@
-package com.example.kafkaconsumerproject;
+package com.example.kafkaconsumerproject.service;
 
 import com.example.kafkaconsumerproject.entity.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerOrder {
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumerOrder.class);
 
     @KafkaListener(topics = "orders")
     public void listen(Order order) {
-        System.out.println(
+        log.info(
                 "Новый заказ: " + order.getOrderId() +
                         " для " + order.getCustomerName() +
                         ", сумма: " + order.getTotalAmount()
